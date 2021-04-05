82aa666f71b8808e338e3622b40772961b02e5cc
Dmitriy <carnagery@mail.ru>
2021-04-05 20:29:32 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    pagination: true,
+    dots: true,
+    // responsive: {
+    //     0: {
+    //         items: 1,
+    //     },
+    //     600: {
+    //         items: 3,
+    //     },
+    //     1000: {
+    //         items: 5,
+    //     },
+    // },
+});
