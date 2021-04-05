a3fe86c5c14decaa8d763bd09dc62f5fd47d5b49
Dmitriy <carnagery@mail.ru>
2021-04-05 20:40:43 +0300

    carousel fix

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ $(".owl-carousel").owlCarousel({
     nav: true,
     pagination: true,
     dots: true,
-    // responsive: {
-    //     0: {
-    //         items: 1,
-    //     },
-    //     600: {
-    //         items: 3,
-    //     },
-    //     1000: {
-    //         items: 5,
-    //     },
-    // },
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 3,
+        },
+        1000: {
+            items: 5,
+        },
+    },
 });
