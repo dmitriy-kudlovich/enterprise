d5985bdc3814f02f717eb64e48fb1f61f87b24f1
Dmitriy Kudlovich <carnagery@mail.ru>
2021-04-18 03:36:14 +0300

    header mobile ready

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    loop: false,
-    margin: 10,
-    pagination: true,
-    stagePadding: 50,
-    responsiveClass: true,
-    dotsEach: 3,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      480: {
-        items: 3,
-      },
-    },
-  });
+const navMobile = document.querySelector("#navMobile");
+const hamburger = document.querySelector("#hamburger");
+
+navMobile.addEventListener("click", () => {
+  navMobile.style.right = "-100%";
+});
+
+hamburger.addEventListener("click", () => {
+  navMobile.style.right = "0";
 });
